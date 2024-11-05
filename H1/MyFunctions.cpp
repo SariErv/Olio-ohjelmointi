9405e282828429e1f0ceb4d2069168773ba5aee6
Sari Ervasti <t3ersa@students.oamk.fi>
2024-11-05 19:52:12 +0200

    Valmis H1 tehtävä

--- H1/MyFunctions.cpp
+++ H1/MyFunctions.cpp
@@ -1,14 +1,47 @@
+#include <iostream>
+#include <MyFunctions.h>
 
-int kertoma(int n)
-{
-    if (n<0){
-        return -1;
+using namespace std;
+
+// int kertoma(int n)
+// {
+//     if (n<0){
+//         return -1;
+//     }
+//     else{
+//         int tulos=1;
+//         for(int row=1; row<=n; row ++){
+//             tulos=row*tulos;
+//         }
+//         return tulos;
+//     }
+// }
+
+
+void calcSum(int a, int b) {
+    int summa = a + b;
+    cout << "Luvun " <<a<< " ja luvun "<<b<< " summa on " <<summa<<endl;
+}
+
+void calcDiv(int a, int b) {
+    if (b == 0) {
+        cout << "Virheilmoitus" << endl;
+    } else {
+        int osamaara = a / b;
+        cout << "Luvun " <<a<< " ja luvun "<<b<< " osamaara on " <<osamaara<<endl;
     }
-    else{
-        int tulos=1;
-        for(int row=1; row<=n; row ++){
-            tulos=row*tulos;
-        }
-        return tulos;
+}
+
+int retSum(int a, int b) {
+    return a + b;
+}
+
+
+float retDiv(int a, int b) {
+    if (b == 0) {
+        cout << "Virheilmoitus" << endl;
+        return 0;
+    } else {
+      return static_cast<float>(a) / b;
     }
 }
--- H1/main.cpp
+++ H1/main.cpp
@@ -1,28 +1,50 @@
+#include <iomanip>
 #include <iostream>
 #include <MyFunctions.h>
 
 using namespace std;
 
+
+
 int main()
 {
-    int num;
-    int tulos;
+    // int num;
+    // int tulos;
+    int a, b;
 
-    cout << "Lasketaan kertoma!" << endl;
-    cout << "Anna positiivinen kokonaisluku" <<endl;
-    cin >> num;
+    // cout << "Lasketaan kertoma!" << endl;
+    // cout << "Anna positiivinen kokonaisluku" <<endl;
+    // cin >> num;
 
-    tulos=kertoma(num);
+    // tulos=kertoma(num);
 
-    if (tulos == -1){
+    // if (tulos == -1){
 
-        cout<<"kertomaa ei voi laskea negatiiviselle luvulle"<<endl;
+    //     cout<<"kertomaa ei voi laskea negatiiviselle luvulle"<<endl;
 
-    }else{
+    // }else{
 
-        cout << "Luvun " <<num<< "kertoma ="<<kertoma(num)<<endl;
+    //     cout << "Luvun " <<num<< "kertoma ="<<kertoma(num)<<endl;
 
-    }
+    // }
+
+
+
+    cout<<"Anna kokonaisluku"<<endl;
+    cin >> a;
+
+    cout<<"Anna toinen kokonaisluku"<<endl;
+    cin >> b;
+
+    int summa = retSum(a, b);
+    float osamaara = retDiv(a, b);
+
+    cout << "retSum palauttama summa: " << summa << endl;
+    cout << fixed << setprecision(2);
+    cout << "retDiv palauttama osamaara: " << osamaara << endl;
+
 
     return 0;
+
+
 }
